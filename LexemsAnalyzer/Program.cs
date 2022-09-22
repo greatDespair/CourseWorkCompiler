ff65da760d07a8ecdd5af29d6e5fcfd322651004
Sergey <akmashev2002@gmail.com>
2022-09-22 22:25:24 +0300

    Добавьте файлы проекта.

---
+++ LexemsAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using LexemsAnalyzer;
+
+AnalyzeText LexemsAnalyzer = new AnalyzeText(Directory.GetCurrentDirectory() + "/Program.txt");
+Console.WriteLine(LexemsAnalyzer.FileText);
+if (LexemsAnalyzer.Analyze())
+{
+     var lexems = LexemsAnalyzer.GetLexemsAsList();
+     foreach (var lexem in lexems)
+     {
+        Console.Write(lexem.Type + " ");
+     }
+}
